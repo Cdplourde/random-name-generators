0fd23da07f509ef09aae7df091fe5516e946849d
cdplourde <cdplourde@protonmail.com>
2022-03-28 01:32:40 -0700

    update connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,13 +2,15 @@ import { variables } from '$lib/variables';
 import pg from 'pg'
 const Pool = pg.Pool
 
-const pool = new Pool({
+const config = process.env.DB_USER ? 'postgresql://db-random-name-generators:w5amfX3mG9lFz4VH@app-b2225c53-4b10-4b8e-88c2-b2ce72d77ab8-do-user-9139609-0.b.db.ondigitalocean.com:25060/db-random-name-generators?sslmode=require' : {
   user: variables.DB_USER,
   host: variables.DB_HOST,
   database: variables.DB_NAME,
   password: variables.DB_PASSWORD,
   port: variables.DB_PORT,
   ssl: variables.DB_SSL == 'false' ? false : true
-})
+}
+
+const pool = new Pool(config)
 
 export default pool;
