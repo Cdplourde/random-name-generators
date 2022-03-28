078603c912a8f7f9d29cbdda426e373eccc15cc5
cdplourde <cdplourde@protonmail.com>
2022-03-28 14:56:39 -0700

    added blog section spacing

--- src/routes/blog/tips-for-choosing-first-name.svelte
+++ src/routes/blog/tips-for-choosing-first-name.svelte
@@ -3,57 +3,76 @@
   <meta name="description" content="Struggling to decide on the perfect name? We’re here to help you brainstorm names, narrow down your list, and find the perfect fit.">
 </svelte:head>
 
-<h2>9 Tips for Choosing the Perfect First Name</h2>
-<p>It’s a monumental decision. Whether it’s a newborn’s name, your own new identity, or the lead protagonist in your latest novel, choosing a first name is something deeply personal that could last a lifetime.</p>
-<p>All that pressure builds up to find the <em>perfect</em> name. It’s no wonder that choosing a name can be terrifying and challenging! You might find plenty of potential candidates while exploring our <a href="/g/boy">boy names</a> and <a>girl names</a> generators, but narrowing down your options is the real challenge.</p>
-<p>If you find yourself struggling to decide on a name for a baby, character, or yourself, here are some tricks you can use to smooth along the process.</p>
-
-<h3>1. Make a huge list of names, then slowly narrow it down</h3>
-<p>When you first start brainstorming, it’s best to just go free and be open to your options. Don’t worry about finding the one and only perfect name right away.</p>
-<p>Using a tool like our <a href="/g/first">first name generator</a>, it only takes a few clicks to be exposed to hundreds of different names. If any catch your eye, even a little, write it down.</p>
-<p>This clears out your short-term memory so your brain can process new name suggestions without getting caught up on ones you’re already considering.</p>
-<p>It can take time to decide on a name that truly clicks. Don’t rush the process. Enjoy the discovery!</p>
-
-<h3>2. Explore name meanings</h3>
-<p>Some names are rooted in meaning that can add depth and meaning to your chosen name. There are plenty of name origin websites out there (such as <a href="https://www.behindthename.com/">Behind the Name</a> and <a href="https://www.names.org/">Names.org</a>), so take a look!</p>
-
-<h3>3. Brainstorm possible personal or historical inspirations for a name</h3>
-<p>Naming children after parents or grandparents is a common way to honor our ancestors and pass on a family name. You can also consider any fictional characters or historical figures who inspired you or impacted your life.</p>
-<p>Choosing a name this way gives the name more depth and significance. For the sake of your child, you may want to avoid shoehorning a political statement into their name. Still, a from-the-heart inspiration can be a subtle but meaningful nod toward your role models and core values.</p>
-
-<h3>4. Research who else might share the same name</h3>
-<p>History is filled with impactful individuals, both good and bad. It can be a double-edged sword!</p>
-<p>If you choose a name based on a positive influence from tip #3, you might not have to worry about this. But if you’re considering names you stumbled upon or found by researching name meanings, it’s worth your time to dig a little deeper.</p>
-<p>It only takes a few seconds to do your due diligence and look up the names you’re considering. Scroll through a few pages of search results just to familiarize yourself with what kind of weight the name might hold.</p>
-
-<h3>5. Consider how common or rare specific names are</h3>
-<p>If you’re a writer choosing character names for a story, you’re going to need a lot of names. Assigning every character a common name just won’t work because they may start to blend together and confuse your reader. At the same time, if every character has a wild, unheard-of name, it will feel overwhelming. Look into adding a variety of names.</p>
-<p>Name rarity can be an essential factor when naming a child, especially in grade school. Common names can be frustrating for a child when multiple classmates share the same name. At the same time, names that are too unique can be challenging for children to spell or pronounce, leading to bullying.</p>
-<p>Grade school is only one phase of a child’s life, so your decision doesn’t have to be based on this alone. It’s another factor to consider as part of the larger picture.</p>
-<p>Nicknames can also help immensely with both sides of this problem, which leads to our next tip for choosing a name...</p>
-
-<h3>6. Think about any possible nicknames</h3>
-<p>Nicknames are fun, cute, and personal. Choosing a first name that’s nickname-friendly can add a lot of flexibility and fun!</p>
-<p>In the case of babies, it gives your child some say in what they want to be called. Your newborn baby isn’t able to give any input on choosing their name, but a nickname lets them add their own personal flair.</p>
-<p>Plus, as mentioned above, nicknames can be incredibly helpful during early school years. Unique names can have a nickname that’s easier for young children to spell, and common names can have a nickname that helps a child stand out from their classmates.</p>
-<p>In the case of fictional characters, nicknames open up a ton of opportunities for unique interactions and relationships. For example, nicknames that are only used by one other character can be a sweet way to show a relationship specific to those two.</p>
-
-<h3>7. Consider how the first and last name works together.</h3>
-<p>Often, characters in fiction have alliterative names. Think of superheroes such as Peter Parker or Clark Kent. This makes the name catchy and memorable!</p>
-<p>You may or may not want to take this route when naming a baby or character. Wacky wordplay can come off as cartoonish if you aren’t careful. It’s one of those naming techniques that can go either way.</p>
-<p>Say the first and last names out loud to see how they sound together. Say it a few times and in different contexts—a question, a statement, a shout. Is it easy to say? Do you trip over your tongue?</p>
-<p>Naming a character and need help choosing a last name? Check out our <a href="/g/last">last name generator</a>!</p>
-
-<h3>8. Write out the name’s initials.</h3>
-<p>If you’re torn between a handful of names, a trick like this can help you trim down your list and choose!</p>
-<p>When you add a middle name, there’s a lot of opportunity for some very unfortunate 3-letter initial combinations. Do the initials spell out a word? Is it a negative or unprofessional word? Especially when it comes to naming children, do your future kid a favor by avoiding initials like DIE or TIT.</p>
-
-<h3>9. Sleep on it.</h3>
-<p>When you spend a lot of time looking at the same name, it can start to sound distorted and strange. It’s good to take breaks!</p>
-<p>For such an important decision, choosing a name shouldn’t be rushed. Sometimes it’s also good to sleep on a name and see how it sounds the next morning.</p>
-<p>Sharing the names with friends or family can be an excellent way to gather feedback, just be sure to take everyone’s opinions with a grain of salt. Don’t let someone dissuade you from a name you truly love just because they knew a guy in high school with the same name and he was a jerk. It’s ultimately your decision!</p>
+<div class="blog-container">
+  <div class="blog-section">
+    <h2>9 Tips for Choosing the Perfect First Name</h2>
+    <p>It’s a monumental decision. Whether it’s a newborn’s name, your own new identity, or the lead protagonist in your latest novel, choosing a first name is something deeply personal that could last a lifetime.</p>
+    <p>All that pressure builds up to find the <em>perfect</em> name. It’s no wonder that choosing a name can be terrifying and challenging! You might find plenty of potential candidates while exploring our <a href="/g/boy">boy names</a> and <a>girl names</a> generators, but narrowing down your options is the real challenge.</p>
+    <p>If you find yourself struggling to decide on a name for a baby, character, or yourself, here are some tricks you can use to smooth along the process.</p>
+  </div>
+  <div class="blog-section">
+    <h3>1. Make a huge list of names, then slowly narrow it down</h3>
+    <p>When you first start brainstorming, it’s best to just go free and be open to your options. Don’t worry about finding the one and only perfect name right away.</p>
+    <p>Using a tool like our <a href="/g/first">first name generator</a>, it only takes a few clicks to be exposed to hundreds of different names. If any catch your eye, even a little, write it down.</p>
+    <p>This clears out your short-term memory so your brain can process new name suggestions without getting caught up on ones you’re already considering.</p>
+    <p>It can take time to decide on a name that truly clicks. Don’t rush the process. Enjoy the discovery!</p>
+  </div>
+  <div class="blog-section">
+    <h3>2. Explore name meanings</h3>
+    <p>Some names are rooted in meaning that can add depth and meaning to your chosen name. There are plenty of name origin websites out there (such as <a href="https://www.behindthename.com/">Behind the Name</a> and <a href="https://www.names.org/">Names.org</a>), so take a look!</p>    
+  </div>
+  <div class="blog-section">
+    <h3>3. Brainstorm possible personal or historical inspirations for a name</h3>
+    <p>Naming children after parents or grandparents is a common way to honor our ancestors and pass on a family name. You can also consider any fictional characters or historical figures who inspired you or impacted your life.</p>
+    <p>Choosing a name this way gives the name more depth and significance. For the sake of your child, you may want to avoid shoehorning a political statement into their name. Still, a from-the-heart inspiration can be a subtle but meaningful nod toward your role models and core values.</p>
+  </div>
+  <div class="blog-section">
+    <h3>4. Research who else might share the same name</h3>
+    <p>History is filled with impactful individuals, both good and bad. It can be a double-edged sword!</p>
+    <p>If you choose a name based on a positive influence from tip #3, you might not have to worry about this. But if you’re considering names you stumbled upon or found by researching name meanings, it’s worth your time to dig a little deeper.</p>
+    <p>It only takes a few seconds to do your due diligence and look up the names you’re considering. Scroll through a few pages of search results just to familiarize yourself with what kind of weight the name might hold.</p>
+  </div>
+  <div class="blog-section">
+    <h3>5. Consider how common or rare specific names are</h3>
+    <p>If you’re a writer choosing character names for a story, you’re going to need a lot of names. Assigning every character a common name just won’t work because they may start to blend together and confuse your reader. At the same time, if every character has a wild, unheard-of name, it will feel overwhelming. Look into adding a variety of names.</p>
+    <p>Name rarity can be an essential factor when naming a child, especially in grade school. Common names can be frustrating for a child when multiple classmates share the same name. At the same time, names that are too unique can be challenging for children to spell or pronounce, leading to bullying.</p>
+    <p>Grade school is only one phase of a child’s life, so your decision doesn’t have to be based on this alone. It’s another factor to consider as part of the larger picture.</p>
+    <p>Nicknames can also help immensely with both sides of this problem, which leads to our next tip for choosing a name...</p>
+  </div>
+  <div class="blog-section">
+    <h3>6. Think about any possible nicknames</h3>
+    <p>Nicknames are fun, cute, and personal. Choosing a first name that’s nickname-friendly can add a lot of flexibility and fun!</p>
+    <p>In the case of babies, it gives your child some say in what they want to be called. Your newborn baby isn’t able to give any input on choosing their name, but a nickname lets them add their own personal flair.</p>
+    <p>Plus, as mentioned above, nicknames can be incredibly helpful during early school years. Unique names can have a nickname that’s easier for young children to spell, and common names can have a nickname that helps a child stand out from their classmates.</p>
+    <p>In the case of fictional characters, nicknames open up a ton of opportunities for unique interactions and relationships. For example, nicknames that are only used by one other character can be a sweet way to show a relationship specific to those two.</p>
+  </div>
+  <div class="blog-section">
+    <h3>7. Consider how the first and last name works together.</h3>
+    <p>Often, characters in fiction have alliterative names. Think of superheroes such as Peter Parker or Clark Kent. This makes the name catchy and memorable!</p>
+    <p>You may or may not want to take this route when naming a baby or character. Wacky wordplay can come off as cartoonish if you aren’t careful. It’s one of those naming techniques that can go either way.</p>
+    <p>Say the first and last names out loud to see how they sound together. Say it a few times and in different contexts—a question, a statement, a shout. Is it easy to say? Do you trip over your tongue?</p>
+    <p>Naming a character and need help choosing a last name? Check out our <a href="/g/last">last name generator</a>!</p>
+  </div>
+  <div class="blog-section">
+    <h3>8. Write out the name’s initials.</h3>
+    <p>If you’re torn between a handful of names, a trick like this can help you trim down your list and choose!</p>
+    <p>When you add a middle name, there’s a lot of opportunity for some very unfortunate 3-letter initial combinations. Do the initials spell out a word? Is it a negative or unprofessional word? Especially when it comes to naming children, do your future kid a favor by avoiding initials like DIE or TIT.</p>
+  </div>
+  <div class="blog-section">
+    <h3>9. Sleep on it.</h3>
+    <p>When you spend a lot of time looking at the same name, it can start to sound distorted and strange. It’s good to take breaks!</p>
+    <p>For such an important decision, choosing a name shouldn’t be rushed. Sometimes it’s also good to sleep on a name and see how it sounds the next morning.</p>
+    <p>Sharing the names with friends or family can be an excellent way to gather feedback, just be sure to take everyone’s opinions with a grain of salt. Don’t let someone dissuade you from a name you truly love just because they knew a guy in high school with the same name and he was a jerk. It’s ultimately your decision!</p>
+  </div>
+</div>
 
 <style>
+  .blog-section:not(:last-of-type) {
+    margin-bottom: 2.25rem;
+  }
+  .blog-container {
+    margin: 1rem 2rem;
+  }
   h2, h3 {
     color: var(--text-green);
   }
