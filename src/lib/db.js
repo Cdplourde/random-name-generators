ab05155c077b05e8318bc5302220b376503578e9
cdplourde <cdplourde@protonmail.com>
2022-03-28 02:27:13 -0700

    more cxn fixes

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,5 @@
 import '$lib/variables';
 import pg from 'pg'
-if (!process.env.ISDEV) pg.defaults.ssl = true;
 const Pool = pg.Pool
 
 const pool = new Pool()
--- src/lib/variables.js
+++ src/lib/variables.js
@@ -3,4 +3,4 @@ process.env.PGUSER ? '' : process.env.PGUSER = import.meta.env.VITE_PGUSER
 process.env.PGHOST ? '' : process.env.PGHOST = import.meta.env.VITE_PGHOST
 process.env.PGDATABASE ? '' : process.env.PGDATABASE = import.meta.env.VITE_PGDATABASE
 process.env.PGPORT ? '' : process.env.PGPORT = import.meta.env.VITE_PGPORT
-process.env.ISDEV ? '' : process.env.ISDEV = true
+process.env.PGSSLMODE ? '' : process.env.PGSSLMODE = import.meta.env.VITE_PGSSLMODE
