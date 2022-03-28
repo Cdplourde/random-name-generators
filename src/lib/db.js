3b8363474d9550a0c51e096905c067df1528ba2f
cdplourde <cdplourde@protonmail.com>
2022-03-27 23:14:06 -0700

    fix connections

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,9 @@ const pool = new Pool({
   user: variables.DB_USER,
   host: variables.DB_HOST,
   database: variables.DB_NAME,
-  password: variables.DB_PASSWORD
+  password: variables.DB_PASSWORD,
+  port: variables.DB_PORT,
+  ssl: variables.DB_SSL == 'false' ? false : true
 })
 
 export default pool;
--- src/lib/variables.js
+++ src/lib/variables.js
@@ -2,5 +2,7 @@ export const variables = {
   DB_USER: process.env.DB_USER || import.meta.env.VITE_DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD || import.meta.env.VITE_DB_PASSWORD,
   DB_HOST: process.env.DB_HOST || import.meta.env.VITE_DB_HOST,
-  DB_NAME:process.env.DB_NAME || import.meta.env.VITE_DB_NAME
+  DB_NAME:process.env.DB_NAME || import.meta.env.VITE_DB_NAME,
+  DB_PORT: process.env.DB_PORT || import.meta.env.VITE_DB_PORT,
+  DB_SSL: process.env.DB_SSL || import.meta.env.VITE_DB_SSL
 }
