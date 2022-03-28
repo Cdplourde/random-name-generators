daaa6d48a5d57005aa5c45261fc99dc73a668dea
cdplourde <cdplourde@protonmail.com>
2022-03-27 22:37:36 -0700

    add start script

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,6 +1,6 @@
 export const variables = {
-  DB_USER: import.meta.env.VITE_DB_USER,
-  DB_PASSWORD: import.meta.env.VITE_DB_PASSWORD,
-  DB_HOST: import.meta.env.VITE_DB_HOST,
-  DB_NAME: import.meta.env.VITE_DB_NAME
+  DB_USER: process.env.DB_USER || import.meta.env.VITE_DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD || import.meta.env.VITE_DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST || import.meta.env.VITE_DB_HOST,
+  DB_NAME:process.env.DB_NAME || import.meta.env.VITE_DB_NAME
 }
