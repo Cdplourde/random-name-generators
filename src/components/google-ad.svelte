6c4414b6ba1d0be937db374c2260842ee488c067
cdplourde <cdplourde@protonmail.com>
2022-04-02 13:06:18 -0700

    fix ads not changing on page change

--- src/components/google-ad.svelte
+++ src/components/google-ad.svelte
@@ -2,8 +2,19 @@
   import { onMount } from 'svelte';
   import { page } from '$app/stores';
 
+  let initialRun = true;
+
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    page.subscribe(() => {
+      if (!initialRun) {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    })
+  }
+
   onMount(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
+    initialRun = false;
   });
 </script>
 
